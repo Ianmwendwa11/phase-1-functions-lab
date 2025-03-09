601c3a9a23e0c9cd9bfcf9a587d3a519d6b1ee4c
ianmwendwa11 <ian.musyoki12@gmail.com>
2025-03-09 13:45:22 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqStreet = 42;
+    return Math.abs(pickupLocation - hqStreet);
+  }
+
+  function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+  }
+
+  console.log(distanceFromHqInBlocks(60))
+  console.log(distanceFromHqInFeet(60))
+  console.log(distanceTravelledInFeet(60,23))
